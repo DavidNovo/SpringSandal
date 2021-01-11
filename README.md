1cd58bea35a12bff29bfddd1232ea8320224ff84
GitHub <david.novogrodsky@gmail.com>
2021-01-10 21:08:00 -0600

    Adding swagger/OpenAPI documentation to the application.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-SpringSandal
+# SpringSandal
+
+## Introduction
+
+## Operations
+
+### Running the application
+If you use Maven, you can run the application by using 
+    ./mvnw spring-boot:run
+Alternatively, you can build the JAR file with 
+    ./mvnw clean package 
+and then run the JAR file, as follows:
+    java -jar target/gs-rest-service-0.1.0.jar
+
+### Default end points for the applications
+
+#### Actuator 
+These are default endpoints used to monitor the health and characterists of the application.
+
+By default, the endpoints start with the prefix of '/actuator' and append the specific endpoint.  See https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-features.html for a complete list.
+
---
+++ src/main/java/com/example/springsandal/configuration/SpringFoxConfig.java
@@ -0,0 +1,27 @@
+package com.example.springsandal.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * This is the class that connects the Spring Boot application to the
+ * interactive UI created by Swagger.  I am setting the documentation
+ * type to OAS 3.0.
+ *
+ * @author David Novogrodsky
+ */
+@Configuration
+public class SpringFoxConfig {
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.OAS_30)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+  }
+}
